7d9ba26ef1ef53ca700fe4fcff3fb48aca5c8f9b
Jadson <jadson.g-matos@outlook.com>
2022-03-01 16:37:30 -0300

    1

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 let net;
-const classifier = knnClassifier.create();
 
 async function app() {
   console.log("Loading mobilenet..");
 
   // Load the model.
-  net = await mobilenet.load();
+  net = await mobilenet.load({ version: 2, alpha: 1.0 });
   console.log("Successfully loaded model");
 
+  const imgEu = document.getElementById("eu");
+
+  const activation = net.infer(imgEu, true);
+  console.log("eu activation", await activation.array());
+
+
   // Make a prediction through the model on our image.
-  const imgEl = document.getElementById("img");
-  const result = await net.classify(imgEl);
-  console.log(result);
-  console.log(net);
+  const bill = document.getElementById("bill");
+  const inferBill = net.infer(bill, true)
+  console.log("bill activation",await inferBill.array() )
+
+
+
+  const eu2 = document.getElementById("eu2");
+  const inferEu2 = net.infer(eu2, true)
+  console.log("eu2 activation", await inferEu2.array())
 }
 
 app();
