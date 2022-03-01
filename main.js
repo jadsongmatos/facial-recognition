9d10e5ebfc85612cd9c90701fc3e55e8bf822775
Jadson <jadson.g-matos@outlook.com>
2022-03-01 16:48:13 -0300

    2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let net;
+const classifier = knnClassifier.create();
 
 async function app() {
   console.log("Loading mobilenet..");
@@ -8,21 +9,26 @@ async function app() {
   console.log("Successfully loaded model");
 
   const imgEu = document.getElementById("eu");
-
   const activation = net.infer(imgEu, true);
-  console.log("eu activation", await activation.array());
+  //console.log("eu activation", await activation.array());
+  classifier.addExample(activation, 0);
 
 
-  // Make a prediction through the model on our image.
   const bill = document.getElementById("bill");
   const inferBill = net.infer(bill, true)
-  console.log("bill activation",await inferBill.array() )
-
-
+  //console.log("bill activation",await inferBill.array() )
+  classifier.addExample(inferBill,1);
 
   const eu2 = document.getElementById("eu2");
   const inferEu2 = net.infer(eu2, true)
-  console.log("eu2 activation", await inferEu2.array())
+  classifier.addExample(inferEu2,0);
+
+  const eu3 = document.getElementById("eu3");
+  const inferEu3 = net.infer(eu3, 'conv_preds');
+  // Get the most likely class and confidence from the classifier module.
+  const result = await classifier.predictClass(inferEu3);
+  console.log(result)
+  //console.log("eu2 activation", await inferEu2.array())
 }
 
 app();
