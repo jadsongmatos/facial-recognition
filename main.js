5dad7622eff202e18662cd5bc5ffe15e43895f6e
jadson <jadson.g-matos@outlook.com>
2022-03-01 15:38:43 -0300

    0

---
+++ main.js
@@ -0,0 +1,18 @@
+let net;
+const classifier = knnClassifier.create();
+
+async function app() {
+  console.log("Loading mobilenet..");
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log("Successfully loaded model");
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById("img");
+  const result = await net.classify(imgEl);
+  console.log(result);
+  console.log(net);
+}
+
+app();
